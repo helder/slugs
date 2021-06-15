eaf2c05e7de3dd002e3109ce080b97b1a01f8228
Ben Merckx <ben@codeurs.be>
2021-06-15 09:31:10 +0200

    Haxe 4.2.2 - rename to "slugify" - add js package

--- src/helder/Slugs.hx
+++ src/helder/Slugs.hx
@@ -4,46 +4,44 @@ import helder.Set;
 
 using haxe.iterators.StringIteratorUnicode;
 
-class Slugs {
-  static final SEPARATE = new Set([
-    ' '.code,
-    '!'.code,
-    '['.code,
-    ']'.code,
-    ','.code,
-    '.'.code,
-    '/'.code,
-    '\\'.code,
-    ';'.code,
-    '<'.code,
-    '>'.code,
-    '?'.code,
-    '_'.code,
-    '-'.code,
-    '|'.code,
-    '('.code,
-    ')'.code,
-    '#'.code,
-    "'".code,
-    '*'.code,
-    '{'.code,
-    '}'.code
-  ]);
+final SEPARATE = new Set([
+  ' '.code,
+  '!'.code,
+  '['.code,
+  ']'.code,
+  ','.code,
+  '.'.code,
+  '/'.code,
+  '\\'.code,
+  ';'.code,
+  '<'.code,
+  '>'.code,
+  '?'.code,
+  '_'.code,
+  '-'.code,
+  '|'.code,
+  '('.code,
+  ')'.code,
+  '#'.code,
+  "'".code,
+  '*'.code,
+  '{'.code,
+  '}'.code
+]);
 
-  public static function slug(str: String, separator = '-'): String {
-    var current = new StringBuf();
-    final result = [];
-    inline function push() {
-      if (current.length == 0) return;
-      result.push(current.toString());
-      current = new StringBuf();
-    }
-    for (code in str.toLowerCase().unicodeIterator()) {
-      if (code < ' '.code) continue;
-      if (SEPARATE.exists(code)) push()
-      else current.addChar(code);
-    }
-    push();
-    return result.join(separator);
+@:expose function slugify(string: String, separator = '-'): String {
+  var current = new StringBuf();
+  final result = [];
+  inline function push() {
+    if (current.length == 0) return;
+    result.push(current.toString());
+    current = new StringBuf();
   }
+  for (code in string.toLowerCase().unicodeIterator()) {
+    if (code < ' '.code) continue;
+    if (SEPARATE.exists(code)) push()
+    else current.addChar(code);
+  }
+  push();
+  return result.join(separator);
 }
--- tests/TestSlugs.hx
+++ tests/TestSlugs.hx
@@ -1,24 +1,24 @@
 package tests;
 
-import helder.Slugs.slug;
+import helder.Slugs.slugify;
 
 @:asserts
 class TestSlugs {
   public function new() {}
 
   public function testSlugs() {
-    asserts.assert(slug('A') == 'a');
-    asserts.assert(slug('A ') == 'a');
-    asserts.assert(slug(' A ') == 'a');
-    asserts.assert(slug(' -A- ') == 'a');
-    asserts.assert(slug(' -A - ') == 'a');
+    asserts.assert(slugify('A') == 'a');
+    asserts.assert(slugify('A ') == 'a');
+    asserts.assert(slugify(' A ') == 'a');
+    asserts.assert(slugify(' -A- ') == 'a');
+    asserts.assert(slugify(' -A - ') == 'a');
 
-    asserts.assert(slug('a b') == 'a-b');
-    asserts.assert(slug('a -b') == 'a-b');
-    asserts.assert(slug('a -,.b') == 'a-b');
-    asserts.assert(slug('a -,.b-)') == 'a-b');
+    asserts.assert(slugify('a b') == 'a-b');
+    asserts.assert(slugify('a -b') == 'a-b');
+    asserts.assert(slugify('a -,.b') == 'a-b');
+    asserts.assert(slugify('a -,.b-)') == 'a-b');
 
-    asserts.assert(slug('Лорем ипсум') == 'лорем-ипсум');
+    asserts.assert(slugify('Лорем ипсум') == 'лорем-ипсум');
 
     return asserts.done();
   }
